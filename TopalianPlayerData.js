a42876df734178553cfbdd9340fd03cf8e0d92aa
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2021-10-25 22:57:02 -0400

    Add files via upload

---
+++ TopalianPlayerData.js
@@ -0,0 +1,15 @@
+playerArray=[
+ {
+  "name": "Christopher",
+  "id": "thePlayer",
+  "posX": 400,
+  "posY": 710,
+  "theWidth": 70,
+  "theHeight": 70,
+  "colorFill": "rgb(73, 100, 150)",
+  "colorText": "rgb(255, 255, 255)",
+  "opacity": 1.0,
+  "texture": "url(\"https://collegeofscripting.weebly.com/uploads/6/4/4/8/64482293/concrete1.jpg\")"
+ }
+];
+
---
+++ TopalianStructureData.js
@@ -0,0 +1,87 @@
+structuresArray=[
+ {
+  "name": "House 1",
+  "id": "STRUCTURE1",
+  "posX": 246,
+  "posY": 2156,
+  "sizeX": 110,
+  "sizeY": 45,
+  "colorFill": "black",
+  "colorText": "white",
+  "opacity": 1,
+  "texture": "Default"
+ },
+ {
+  "name": "House 2",
+  "id": "STRUCTURE2",
+  "posX": 241,
+  "posY": 3125,
+  "sizeX": 110,
+  "sizeY": 45,
+  "colorFill": "black",
+  "colorText": "white",
+  "opacity": 1,
+  "texture": "Default"
+ },
+ {
+  "name": "Our Third House",
+  "id": "STRUCTURE3",
+  "posX": 232,
+  "posY": 4025,
+  "sizeX": 110,
+  "sizeY": 45,
+  "colorFill": "black",
+  "colorText": "white",
+  "opacity": 1,
+  "texture": "Default"
+ },
+ {
+  "name": "House 4",
+  "id": "STRUCTURE4",
+  "posX": 466,
+  "posY": 578,
+  "sizeX": 114,
+  "sizeY": 49,
+  "colorFill": "rgb(150, 50, 100)",
+  "colorText": "rgb(255, 255, 255)",
+  "opacity": "1",
+  "texture": "none"
+ },
+ {
+  "name": "House 5",
+  "id": "STRUCTURE5",
+  "posX": 679,
+  "posY": 581,
+  "sizeX": 114,
+  "sizeY": 49,
+  "colorFill": "rgb(0, 0, 0)",
+  "colorText": "rgb(100, 200, 255)",
+  "opacity": "1",
+  "texture": "none"
+ },
+ {
+  "name": "House 6",
+  "id": "STRUCTURE6",
+  "posX": 884,
+  "posY": 581,
+  "sizeX": 114,
+  "sizeY": 49,
+  "colorFill": "rgb(0, 0, 0)",
+  "colorText": "rgb(255, 255, 255)",
+  "opacity": "1",
+  "texture": "url(\"https://collegeofscripting.weebly.com/uploads/6/4/4/8/64482293/concrete1.jpg\")"
+ },
+ {
+  "name": "House 7",
+  "id": "STRUCTURE7",
+  "posX": 1156,
+  "posY": 524.5,
+  "sizeX": 114,
+  "sizeY": 49,
+  "colorFill": "rgb(0, 0, 0)",
+  "colorText": "rgb(255, 255, 255)",
+  "opacity": "1",
+  "texture": "none"
+ }
+];
+
